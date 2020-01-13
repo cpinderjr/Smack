610adeb560bf90fcf32df3c64a6a1e7e90cef0f7
Carlos Pinder <carlos.pinder@icloud.com>
2020-01-13 10:59:04 -0500

    Lesson 4 in 3. Fixed broken cocoa pod. Added the login screen and ability to close it. Fixed the password field to use secure text entry.

---
+++ Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Carlos Pinder on 1/7/20.
+//  Copyright © 2020 Carlos Pinder. All rights reserved.
+//
+
+import Foundation
+
+// Seques
+let TO_LOGIN = "toLogin"
